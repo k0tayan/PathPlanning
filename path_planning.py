74e0a04e96187853243e5a09b2df01f5d6e794e4
k0tayan <kotayan0415@outlook.jp>
2018-08-22 16:14:29 +0900

    [update] 表示用のaxをコンストラクタで指定 mainのカウントを削除

--- path_planning.py
+++ path_planning.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import random
 from bottleflip.objects import Robot, Field, Table, Path, Point
 from tcp.Tcp import Tcp
+import sys
 
 random_move = True
 send = False
@@ -16,7 +17,7 @@ move_table_width = 500
 field_width, field_height = 5000, 8500
 move_table_randomize_area_min, move_table_randomize_area_max = 1250, 3750
 
-def main(count=0):
+def main():
     # plot setting
     plt.figure()
     ax = plt.axes()
@@ -29,21 +30,21 @@ def main(count=0):
 
     # create instance
     robot = Robot(robot_width)
-    field = Field(field_width, field_height)
+    field = Field(field_width, field_height, ax)
     if random_move:
-        table_under = Table(random.randint(move_table_randomize_area_min, move_table_randomize_area_max+1), 5500, move_table_width, robot_width)
-        table_middle = Table(random.randint(move_table_randomize_area_min, move_table_randomize_area_max+1), 6500, move_table_width, robot_width)
-        table_up = Table(random.randint(move_table_randomize_area_min, move_table_randomize_area_max+1), 7500, move_table_width, robot_width)
+        table_under = Table(random.randint(move_table_randomize_area_min, move_table_randomize_area_max+1), 5500, move_table_width, robot_width, ax)
+        table_middle = Table(random.randint(move_table_randomize_area_min, move_table_randomize_area_max+1), 6500, move_table_width, robot_width, ax)
+        table_up = Table(random.randint(move_table_randomize_area_min, move_table_randomize_area_max+1), 7500, move_table_width, robot_width, ax)
         print(table_under.x, table_middle.x, table_up.x)
     else:
-        table_under = Table(2500, 5500, move_table_width, robot_width)
-        table_middle = Table(2500, 6500, move_table_width, robot_width)
-        table_up = Table(2500, 7500, move_table_width, robot_width)
+        table_under = Table(2500, 5500, move_table_width, robot_width, ax)
+        table_middle = Table(2500, 6500, move_table_width, robot_width, ax)
+        table_up = Table(2500, 7500, move_table_width, robot_width, ax)
     if zone == 'red':
-        two_stage_table = Table(two_stage_table_red_zone_x, two_stage_table_red_zone_y, two_stage_table_width, robot_width)
+        two_stage_table = Table(two_stage_table_red_zone_x, two_stage_table_red_zone_y, two_stage_table_width, robot_width, ax)
         two_stage_table.set_goal('LEFT')
     else: # zone == 'blue
-        two_stage_table = Table(two_stage_table_blue_zone_x, two_stage_table_blue_zone_y, two_stage_table_width, robot_width)
+        two_stage_table = Table(two_stage_table_blue_zone_x, two_stage_table_blue_zone_y, two_stage_table_width, robot_width, ax)
         two_stage_table.set_goal('RIGHT')
     path = Path(field=field,
                 robot=robot,
@@ -74,11 +75,6 @@ def main(count=0):
     print(path.get_flip_point())
 
     # plot
-    field.set_ax(ax)
-    table_under.set_ax(ax)
-    table_middle.set_ax(ax)
-    table_up.set_ax(ax)
-    two_stage_table.set_ax(ax)
     field.plot()
     two_stage_table.plot()
     table_under.plot()
@@ -87,11 +83,9 @@ def main(count=0):
 
     ax.plot(points_x, points_y, '.', color='red')
     ax.plot(points_x, points_y, '-', color='green')
-    if count != 0:
-         plt.savefig(f'output/figure{count}.png')
     plt.show()
 
 
 if __name__ == '__main__':
-    main(0)
+    main()
 
