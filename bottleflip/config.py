0c15a76de19f83601588c20a9660b653fdad2e67
k0tayan <kotayan0415@outlook.jp>
2018-08-27 22:37:59 +0900

    [add] path_planning.pyのグローバルな設定を移行

---
+++ bottleflip/config.py
@@ -0,0 +1,7 @@
+robot_width = 1200
+two_stage_table_red_zone_x, two_stage_table_red_zone_y = 3000, 3500
+two_stage_table_blue_zone_x, two_stage_table_blue_zone_y = 2000, 3500
+two_stage_table_width = 800
+move_table_width = 500
+field_width, field_height = 5000, 8500
+move_table_randomize_area_min, move_table_randomize_area_max = 1250, 3750
