da368891910cb9cea95643d56cc1f18b3672669a
k0tayan <kotayan0415@outlook.jp>
2018-08-30 09:38:52 +0900

    [update] config.py

--- rsd/Config.py
+++ rsd/Config.py
@@ -1,5 +1,5 @@
 class Config:
-    partition_1 = 268
-    partition_2 = 360
-    count = 300 # 移動平均の取る個数
+    partition_1 = 264
+    partition_2 = 374
+    count = 600 # 移動平均の取る個数
     nud, nmd, nup = 0, 0, 0 # 表示用
