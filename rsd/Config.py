15ee27b91b6728b0982cd9d47297ef3fbb24993e
k0tayan <kotayan0415@outlook.jp>
2018-09-07 14:17:17 +0900

    [update] 深度か中心座標を選べるようにした 設定ファイルのパスを変数で扱うようにした

--- rsd/Config.py
+++ rsd/Config.py
@@ -1,5 +1,7 @@
 class Config:
+    mode = 0 #mode=0: 深度 mode=1 中心座標
     partition_1 = 264
     partition_2 = 374
     count = 600 # 移動平均の取る個数
     nud, nmd, nup = 0, 0, 0 # 表示用
+    setting_path = '/../settings.json'
