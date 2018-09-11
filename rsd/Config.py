1883bde4baf90602dbcd6ad2f07fa2f3e63b5d8f
k0tayan <kotayan0415@outlook.jp>
2018-09-11 17:53:44 +0900

    [update] zone: ゾーン mode: 深度 use_moving_average: 移動平均を使うか
    class Path: 近似関数を作成するためにサンプリングしたデータ

--- rsd/Config.py
+++ rsd/Config.py
@@ -1,7 +1,19 @@
 class Config:
-    mode = 0 #mode=0: 深度 mode=1 中心座標
+    zone = 0 # zone=1 赤ゾーン zone=0 青ゾーン
+    mode = 1 # mode=0: 深度 mode=1 中心座標
+    use_moving_average = 0 # 中心座標で距離を推定する場合で、移動平均を使うか　使う=1 使わない=0
     partition_1 = 264
     partition_2 = 374
     count = 600 # 移動平均の取る個数
     nud, nmd, nup = 0, 0, 0 # 表示用
     setting_path = '/../settings.json'
+
+
+class Path:
+    blue_under = None
+    blue_middle = '/Users/sho/PycharmProjects/PathPlanning/measurements/blue/middle.csv'
+    blue_up = '/Users/sho/PycharmProjects/PathPlanning/measurements/blue/up.csv'
+
+    red_under = None
+    red_middle = None
+    red_up = None
