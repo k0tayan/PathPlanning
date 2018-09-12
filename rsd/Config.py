2fcf0696cea3326095ffabfaea94f85f7471a0d0
k0tayan <kotayan0415@outlook.jp>
2018-09-12 12:36:53 +0900

    [update] Path: blue_underのパスを設定 Fieldクラスを追加

--- rsd/Config.py
+++ rsd/Config.py
@@ -1,19 +1,23 @@
 class Config:
     zone = 0 # zone=1 赤ゾーン zone=0 青ゾーン
     mode = 1 # mode=0: 深度 mode=1 中心座標
-    use_moving_average = 0 # 中心座標で距離を推定する場合で、移動平均を使うか　使う=1 使わない=0
+    use_moving_average = 1 # 中心座標で距離を推定する場合で、移動平均を使うか　使う=1 使わない=0
     partition_1 = 264
     partition_2 = 374
-    count = 600 # 移動平均の取る個数
+    count = 300 # 移動平均の取る個数
     nud, nmd, nup = 0, 0, 0 # 表示用
     setting_path = '/../settings.json'
 
 
 class Path:
-    blue_under = None
+    blue_under = '/Users/sho/PycharmProjects/PathPlanning/measurements/blue/under.csv'
     blue_middle = '/Users/sho/PycharmProjects/PathPlanning/measurements/blue/middle.csv'
     blue_up = '/Users/sho/PycharmProjects/PathPlanning/measurements/blue/up.csv'
 
     red_under = None
     red_middle = None
     red_up = None
+
+class Field:
+    FIELD_WIDTH = 5000
+    TABLE_WIDTH = 500
