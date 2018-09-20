ab8df5cdc6c22b0052b4f9c6e6bdc460f0805fd5
k0tayan <kotayan0415@outlook.jp>
2018-09-20 14:31:41 +0900

    [update] field_max_rightを追加 フィールドの右端に存在できるテーブルの最大座標

--- rsd/Config.py
+++ rsd/Config.py
@@ -1,5 +1,5 @@
 class Config:
-    zone = 0 # zone=1 赤ゾーン zone=0 青ゾーン
+    zone = 1 # zone=1 赤ゾーン zone=0 青ゾーン
     mode = 1 # mode=0: 深度 mode=1 中心座標
     side = 0 # 横からとるか side=1　横から撮る side=0 前から撮る
     use_moving_average = 1 # 中心座標で距離を推定する場合で、移動平均を使うか　使う=1 使わない=0
@@ -12,6 +12,7 @@ class Config:
     setting_path = '/settings.json'
     radius_filter_side = (100, 20)
     radius_filter_front = (110, 22)
+    distance_filter_front = (2.4, 5.3)
 
 
 class Path:
@@ -26,6 +27,8 @@ class Path:
     under_front = '/Users/sho/PycharmProjects/PathPlanning/measurements/front/under_front.csv'
     middle_front = '/Users/sho/PycharmProjects/PathPlanning/measurements/front/middle_front.csv'
     up_front = '/Users/sho/PycharmProjects/PathPlanning/measurements/front/up_front.csv'
+
+    field_max_right = '/Users/sho/PycharmProjects/PathPlanning/measurements/front/field_max_right.csv'
 
 class Field:
     FIELD_WIDTH = 5000
