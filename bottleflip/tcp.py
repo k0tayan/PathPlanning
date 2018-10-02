04d52ba9c7551d7859a3981aa0e3781d135655a8
k0tayan <kotayan0415@outlook.jp>
2018-10-02 16:48:31 +0900

    [update] パケットの構造をpacket_structure.mdに移動

--- bottleflip/tcp.py
+++ bottleflip/tcp.py
@@ -1,35 +1,5 @@
 import socket
 import struct
-from bottleflip.objects import Point
-
-"""
-[0] checksum | 0x80
-[1] x[0] >> 7
-[2] x[0] & 0x7f
-[3] x[1] >> 7
-[4] x[2] & 0x7f
-.
-.
-.
-[17] y[0] >> 7
-[18] y[0] & 0x7f
-[19] y[1] >> 7
-[20] y[2] & 0x7f
-.
-.
-.
-[32] y[8] & 0x7f
-[33] flippoint1 0b00Fiiixx F=failed(true == failed) iii=index xx=dir
-[34] flippoint2 0b00Fiiixx F=failed(true == failed) iii=index xx=dir
-[35] flippoint3 0b00Fiiixx F=failed(true == failed) iii=index xx=dir
-[36] flippoint4 0b00Fiiixx F=failed(true == failed) iii=index xx=dir
-
-0 <= index <= 7
-xx:
-'LEFT':00
-'RIGHT':01
-'FRONT':11
-"""
 
 class Tcp:
     def __init__(self, host='192.168.0.14', port=10001):
