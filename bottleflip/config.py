03b8e4ea7d917886187d3b5f6ad377cb02b82b6b
k0tayan <kotayan0415@outlook.jp>
2018-10-20 22:19:40 +0900

    [update] ロボットの幅 [add] 旋回マージン

--- bottleflip/config.py
+++ bottleflip/config.py
@@ -1,7 +1,11 @@
-robot_width = 1300
+robot_width = 1400
+turn_margin = 200
 two_stage_table_red_zone_x, two_stage_table_red_zone_y = 3000, 3500
 two_stage_table_blue_zone_x, two_stage_table_blue_zone_y = 2000, 3500
 two_stage_table_width = 800
 move_table_width = 500
 field_width, field_height = 5000, 8500
-move_table_randomize_area_min, move_table_randomize_area_max = 1250, 3750
+move_table_randomize_area_min, move_table_randomize_area_max = 1250, 3751
+move_table_under_y = 5500
+move_table_middle_y = 6500
+move_table_up_y = 7500
