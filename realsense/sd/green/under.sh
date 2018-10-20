131e203df4c970d5ece7ceeb6215efb0669675a4
k0tayan <kotayan0415@outlook.jp>
2018-10-20 22:39:19 +0900

    [delete] 画像認識に使うファイル群はgitで管理しないことにした

--- realsense/sd/green/under.sh
+++
@@ -1,8 +0,0 @@
-python retrain.py \
-  --bottleneck_dir=bottlenecks \
-  --how_many_training_steps=100 \
-  --model_dir=inception \
-  --summaries_dir=training_summaries/basic \
-  --output_graph=retrained_graph_green_under.pb \
-  --output_labels=retrained_labels_under.txt \
-  --image_dir=under
