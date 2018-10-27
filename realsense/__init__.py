2baed0d36b21f129287734fa8b7203ada75546ef
k0tayan <kotayan0415@outlook.jp>
2018-10-27 19:44:37 +0900

    [update] Typesを追加

--- realsense/__init__.py
+++ realsense/__init__.py
@@ -2,4 +2,5 @@ from .config import *
 from .detection import *
 from .objects import *
 from .utils import *
-__all__ = ['Config', 'Table', 'Tables', 'Utils', 'ApproximationFunction', 'Color']
+from .types import Types
+__all__ = ['Config', 'Table', 'Tables', 'Utils', 'ApproximationFunction', 'Color', 'Types']
