94d1eadb5a625438604afce087cae37c56b643e3
k0tayan <kotayan0415@outlook.jp>
2018-10-29 10:24:37 +0900

    [update] Parameterを追加

--- realsense/__init__.py
+++ realsense/__init__.py
@@ -3,4 +3,4 @@ from .detection import *
 from .objects import *
 from .utils import *
 from .types import Types
-__all__ = ['Config', 'Table', 'Tables', 'Utils', 'ApproximationFunction', 'Color', 'Types']
+__all__ = ['Config', 'Table', 'Tables', 'Utils', 'ApproximationFunction', 'Color', 'Types', 'Parameter']
