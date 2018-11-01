a27c7a727f8cf116b46b70ab12aa8dcbaab4f61d
k0tayan <kotayan0415@outlook.jp>
2018-11-01 17:36:30 +0900

    [delete] plotを削除

--- bottleflip/objects.py
+++ bottleflip/objects.py
@@ -1,12 +1,11 @@
 import numpy as np
-import matplotlib
 
 LEFT = 'LEFT'
 RIGHT = 'RIGHT'
 FRONT = 'FRONT'
 
-RED = 'red'
-BLUE = 'blue'
+RED = 1
+BLUE = 0
 
 class Point:
     def __init__(self, x, y):
@@ -22,39 +21,15 @@ class Robot:
         self.radius = width/2 * np.sqrt(2)
 
 
-class Rect:
-    def __init__(self, x, y, width):
-        self.lupx = x - width / 2
-        self.lupy = y + width / 2
-        self.ludx = x - width / 2
-        self.ludy = y - width / 2
-        self.rudx = x + width / 2
-        self.rudy = y - width / 2
-        self.rupx = x + width / 2
-        self.rupy = y + width / 2
-
-        self.x_list = [self.lupx, self.ludx, self.rudx, self.rupx]
-        self.y_list = [self.lupy, self.ludy, self.rudy, self.rupy]
-
-
-class Table(Rect, Point):
-    def __init__(self, x, y, width, robot_width, ax):
-        super().__init__(x, y, width)
-        self.x = x
-        self.y = y
+class Table(Point):
+    def __init__(self, x, y, width, robot_width):
+        super().__init__(x, y)
         self.width = width
         self.left_goal: Point = Point(x - width / 2 - robot_width / 2, y)
         self.right_goal: Point = Point(x + width / 2 + robot_width / 2, y)
         self.front_goal: Point = Point(x, y - width / 2 - robot_width / 2)
-        self.ax = ax
         self.goal: Point = None
         self.goal_state: str = None
-
-    def plot(self):
-        if self.ax is None:
-            raise Exception('Please set matplotlib ax')
-        self.ax.plot(self.x, self.y, '.', color='black')
-        self.ax.plot(self.x_list + [self.lupx], self.y_list + [self.lupy], '-', color='blue')
 
     def set_goal(self, direction):
         """
@@ -76,14 +51,6 @@ class Table(Rect, Point):
 
 
 class Field:
-    def __init__(self, width, height, ax):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.ax = ax
-
-    def plot(self):
-        if self.ax is None:
-            raise Exception('Please set matplotlib ax')
-        self.ax.plot([0, 0], [0, self.height], '-', color='black')
-        self.ax.plot([self.width, self.width], [0, self.height], '-', color='black')
-        self.ax.plot([self.width / 2, self.width / 2], [0, self.height], linestyle="dotted")
