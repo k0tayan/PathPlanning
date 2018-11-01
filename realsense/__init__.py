2eaf8c7adb2dbfbf5e71f5506049d0946d21a104
k0tayan <kotayan0415@outlook.jp>
2018-11-01 17:36:48 +0900

    [update] Drawを追加

--- realsense/__init__.py
+++ realsense/__init__.py
@@ -3,5 +3,5 @@ from .detection import *
 from .objects import *
 from .utils import *
 from .types import Types
-from .view import FieldView
-__all__ = ['Config', 'Table', 'Tables', 'Utils', 'ApproximationFunction', 'Color', 'Types', 'Parameter', 'FieldView']
+from .view import *
+__all__ = ['Config', 'Table', 'Tables', 'Utils', 'ApproximationFunction', 'Color', 'Types', 'Parameter', 'FieldView', 'Draw']
