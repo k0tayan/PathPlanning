9399878aaf0cd1e47b7801967967242cbe737a8a
k0tayan <kotayan0415@outlook.jp>
2018-11-03 12:02:21 +0900

    [update]　simpleaudioでテスト再生

--- yukari/play.py
+++ yukari/play.py
@@ -2,10 +2,36 @@
 from pydub import AudioSegment
 from pydub.playback import play
 import glob
+import threading
+import simpleaudio
 
-files = glob.glob('./*.wav')
-# 音声ファイルの読み込み
-for file in files:
-	sound = AudioSegment.from_file(file, "wav")
-	# 再生
-	play(sound)
+
+def _play_with_simpleaudio(seg):
+    return simpleaudio.play_buffer(
+        seg.raw_data,
+        num_channels=seg.channels,
+        bytes_per_sample=seg.sample_width,
+        sample_rate=seg.frame_rate
+    )
+
+
+def start_thread(func, arg):
+    thread = threading.Thread(target=func, args=([arg]), daemon=True)
+    thread.start()
+
+path = ''
+file = AudioSegment.from_file(path, 'wav')
+
+# start_thread(_play_with_simpleaudio, file)
+playback = _play_with_simpleaudio(file)
+# play(file)
+
+while True:
+    num = int(input())
+    print(num)
+    if num == 1:
+        playback.stop()
+    if num == 2:
+        playback.wait_done()
+    if num == 3:
+        print(playback.is_playing())
