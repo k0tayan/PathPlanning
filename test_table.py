e191309db1292cd59a26a07bcc9790a9058036e4
k0tayan <kotayan0415@outlook.jp>
2018-11-04 21:36:58 +0900

    [update] トラックバーを動かすことでテーブルの配置を変更できるように

--- test_table.py
+++ test_table.py
@@ -1,17 +1,36 @@
 import cv2
 import numpy as np
+from realsense.view import FieldView
 from path_planning import PathPlanning
+
+def f(x):
+    return x - 1250
 
 planner = PathPlanning(send=False)
 size = 720, 1280, 3
-pos1, pos2, pos3, zone = 1250, 1250, 3750, 0
-move_table_pos = (pos1, pos2, pos3)
-points, flip_points = planner.main((pos1, pos2, pos3, zone))
-from realsense.view import FieldView
-
 v = FieldView()
-img = v.draw_field(move_table_pos, points)
-cv2.namedWindow("img", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("img", img)
-k = cv2.waitKey()
-exit()
+window_name = 'main'
+cv2.namedWindow(window_name)
+cv2.createTrackbar('table_1', window_name, f(1250), f(3750), int)
+cv2.createTrackbar('table_2', window_name, f(1250), f(3750), int)
+cv2.createTrackbar('table_3', window_name, f(1250), f(3750), int)
+cv2.createTrackbar('zone', window_name, 0, 1, int)
+cv2.setTrackbarPos('table_1', window_name, f(2500))
+cv2.setTrackbarPos('table_2', window_name, f(2500))
+cv2.setTrackbarPos('table_3', window_name, f(2500))
+
+while True:
+    pos1 = cv2.getTrackbarPos('table_1', window_name) + 1250
+    pos2 = cv2.getTrackbarPos('table_2', window_name) + 1250
+    pos3 = cv2.getTrackbarPos('table_3', window_name) + 1250
+    zone = cv2.getTrackbarPos('zone', window_name)
+    move_table_pos = (pos1, pos2, pos3)
+    v.zone = zone
+    points, flip_points = planner.main((pos1, pos2, pos3, zone))
+    img = v.draw_field(move_table_pos, points)
+    img = cv2.resize(img, (int(1280 * 0.65), int(720 * 0.65)))
+    cv2.imshow(window_name, img)
+    k = cv2.waitKey(1)
+
+    if k == ord('q'):
+        break
